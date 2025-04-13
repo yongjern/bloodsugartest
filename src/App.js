5cf1a17f0b2d4948379a1b66cb15e98af294a442
Yong Jie Ern <jieernyong1207@gmail.com>
2025-04-13 09:45:44 +0000

    Beta Version (Can't RUN)

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import BloodSugarTracker from "./BloodSugarTracker";
+
+function App() {
+  return (
+    <div className="App">
+      <BloodSugarTracker />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ui/card.js
@@ -0,0 +1,1 @@
+import { Card } from "./components/ui/card"; // 根據具體路徑調整
